baa55b59a69c71c86a0d3c0e5016b61df350f450
Susie Lee <susiel2@illinois.edu>
2017-12-13 03:04:38 -0600

    comments added

--- webscrape3.py
+++ webscrape3.py
@@ -4,9 +4,10 @@ import praw
 import nltk
 import matplotlib.pyplot as plt
 
+#need individual 'api_keys.json' file for authorization
 with open('api_keys.json') as f:
     api_keys = json.loads(f.read())
-    
+
 reddit = praw.Reddit(
     client_id=api_keys['client_id'],
     client_secret=api_keys['client_secret'],
@@ -15,14 +16,18 @@ reddit = praw.Reddit(
     username=api_keys['username']
     # your info here
 )
+
+
+number_of_posts = 10
+#pull data to create dictionary formatted {title of post : all respective comments} for variable number of posts
 reddit_titles_to_comments = {}
-titles_comm_sent = {}
-submissions = reddit.subreddit('all').hot(limit=2)
+submissions = reddit.subreddit('all').hot(limit=number_of_posts)
 stop_words = nltk.corpus.stopwords.words('english')
 stop_words.extend(["https", "could", "even", "like", "get", "would"])
 for submission in submissions:
-    submission.comments.replace_more(limit=0)    
+    submission.comments.replace_more(limit=0)
     reddit_titles_to_comments[submission.title] = [comment.body for comment in submission.comments.list()]
+
 
 def frequencyDistribution(d):
     words = []
@@ -38,6 +43,7 @@ def frequencyDistribution(d):
     my_text = nltk.Text(words)
     my_text.dispersion_plot(most_freq)
 
+
 def frequencyDistributionAll(d):
     for title, post in d.items():
         most_freq = []
@@ -52,9 +58,26 @@ def frequencyDistributionAll(d):
         my_text.dispersion_plot(most_freq)
 
 
-        
+#calculate sentiment proportions for title and respective comments to juxtapose the two
+#source: http://opensourceforu.com/2016/12/analysing-sentiments-nltk/ to understand how to calculate sentiment proportions
+def subjectivity():
+    #dictionary formatted {title of post : average compound sentiment proportion}
+    post = {}
+    #pull all comments, add to dictionary formatted [title of post : all comments for post]
+    sentences = reddit_titles_to_comments[submission.title]
+    sid = SentimentIntensityAnalyzer()
+    #initial total compound
+    compound = 0
+    #loop through each comment
+    for sentence in sentences:
+        #calculate proportions for 'negative', 'neutral', 'positive', and 'compound'
+        ss = sid.polarity_scores(sentence)
+        #add up all compound values
+        compound += (ss['compound'])
+    #create dictionary of tuple values formatted {title of post : (title: average compound sentiment proportion of title, comments: average compound sentiment proportion of comments}
+    post[submission.title] = ("title: " + str(sid.polarity_scores(submission.title)['compound']), "comments: " + str(compound/len(sentences)))
+    return post
+
 frequencyDistribution(reddit_titles_to_comments)
 frequencyDistributionAll(reddit_titles_to_comments)
-# okay so i have currently gathered all the comments from a given submission, but i'm thinking i can do two separate
-# analysis here: one in which i analyze title sentiment to comment sentiment, and another where i analyze
-# comment sentiment to subreddit to determine which subs have the most positive comment community
+subjectivity()
